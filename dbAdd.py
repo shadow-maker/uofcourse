56144b717cf43bb67eae753729ac33f2d1be8eec
Carlos Andres <ca@carlosandres-mac.local>
2022-01-20 15:10:50 -0500

    Update dbAdd script

--- dbAdd.py
+++ dbAdd.py
@@ -1,7 +1,27 @@
 from planner import db
-from planner.models import Season, Term
+from planner.models import Role, Season, Term
 
 import json
+
+roles = {
+	1 : "user",
+	2 : "moderator",
+	3 : "admin"
+}
+
+for id, name in roles.items():
+	role = Role.query.filter_by(id=id).first()
+	if role:
+		print(f"ROLE {id} ALREADY EXISTS")
+		if role.name != name:
+			print(f"  - name does not match: (db) {role.name} != {name}, updating...")
+			role.name = name
+			db.session.commit()
+	else:
+		print(f"CREATING ROLE {id}")
+		role = Role(id=id, name=name)
+		db.session.add(role)
+		db.session.commit()
 
 termsFile = "backups/terms.json"
 
