40e546abd7c132b46d946dfc7bc436b176c42477
Carlos Andres <carlos.andres.montoro@gmail.com>
2022-02-27 22:20:55 -0700

    Quick fix: redirect functions

--- planner/routes/utils.py
+++ planner/routes/utils.py
@@ -3,8 +3,8 @@ from flask.helpers import url_for
 
 def redirectLogin():
 	flash(f"You need to log in first!", "warning")
-	return redirect(url_for("auth.login"))
+	return redirect(url_for("view.login"))
 
 def redirectNoaccess():
 	flash(f"You do not have permission to access this page!", "warning")
-	return redirect(url_for("main.viewHome"))
+	return redirect(url_for("view.home"))
