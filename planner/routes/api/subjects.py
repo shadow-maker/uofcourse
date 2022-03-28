26ab7abf90c42ae85d2416b11d3c6e44c28c987a
Carlos Andres <carlos.andres.montoro@gmail.com>
2022-03-28 13:36:17 -0600

    Support for /id/courses for subjects and faculties api endpoints

--- planner/routes/api/subjects.py
+++ planner/routes/api/subjects.py
@@ -1,7 +1,11 @@
 from planner.models import Subject
+from planner import queryUtils as utils
 from planner.routes.api.utils import *
+from planner.routes.api.courses import getCoursesFilter
 
 from flask import Blueprint, request
+
+import json
 
 subject = Blueprint("subjects", __name__, url_prefix="/subjects")
 
@@ -17,6 +21,20 @@ def getSubjects():
 @subject.route("/<id>", methods=["GET"])
 def getSubjectById(id):
 	return getById(Subject, id)
+
+@subject.route("/<id>/courses", methods=["GET"])
+def getSubjectCourses(id):
+	if not utils.getById(Subject, id):
+		return {"error": f"Subject with id {id} does not exist"}, 404
+	return getCoursesFilter(data={
+		"sort": request.args.get("sort", default=0, type=int),
+		"asc": request.args.get("asc", default="true", type=str).lower(),
+		"levels": json.loads(request.args.get("levels", default="[]", type=str)),
+		"faculties": [],
+		"subjects": [int(id)],
+		"limit": request.args.get("limit", default=30, type=int),
+		"page": request.args.get("page", default=1, type=int)
+	})
 
 @subject.route("/code/<code>", methods=["GET"])
 def getSubjectByCode(code):
