d705842358304ea169ac2143890f8bbc34576267
Carlos Andres <carlos.andres.montoro@gmail.com>
2022-04-28 13:56:47 -0600

    Support for searching multiple course numbers

--- app/static/scripts/courseBrowser.js
+++ app/static/scripts/courseBrowser.js
@@ -30,12 +30,12 @@ function checkAll(container) {
 
 function requestResults(callback) {
 	let name = ""
-	let number = null
+	let number = []
 
 	let words = $("#searchCourses").val().split(" ")
 	for (let word of words) {
 		if (!isNaN(word) && parseInt(word) >= 100 && parseInt(word) < 800)
-			number = parseInt(word)
+			number.push(parseInt(word))
 		else
 			name += word + " "
 	}
