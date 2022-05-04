235e4f9a7f532816fc1f5bc0d06206296abf7228
Carlos Andres <carlos.andres.montoro@gmail.com>
2022-05-03 22:14:54 -0600

    v1.0.1 version and changelog

--- app/constants.py
+++ app/constants.py
@@ -5,7 +5,7 @@
 SITE_NAME = "UofCourse"
 SITE_LANG = "en"
 DEF_DESCRIPTION = "UofCourse is a web application that helps students in their program course planning."
-CURRENT_VERSION = "1.0"
+CURRENT_VERSION = "1.0.1"
 
 TIMEZONE = "Canada/Mountain"
 
