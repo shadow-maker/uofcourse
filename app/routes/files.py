1e9eec27036f02ec4651072827ed0971421e368c
Carlos Andres <carlos.andres.montoro@gmail.com>
2022-05-05 02:16:37 -0600

    Add route for api.md

--- app/routes/files.py
+++ app/routes/files.py
@@ -1,8 +1,10 @@
-from app import app
+from app import app, jinja
 
-from flask import Blueprint, send_from_directory
+from flask import Blueprint, Response, send_from_directory
+from flask.helpers import url_for
 
 import os
+import re
 
 #
 # Create file route blueprint with no url prefix
@@ -17,3 +19,20 @@ file = Blueprint("file", __name__, url_prefix="/")
 @file.route("/favicon.ico")
 def favicon():
 	return send_from_directory(app.static_folder, "favicon.ico")
+
+
+@file.route("/api.md")
+def api():
+	# Create jinja2 template from api docs markdown file
+	with open(os.path.join(app.static_folder, "api.md"), "r", encoding="utf-8") as file:
+		template = jinja.from_string(file.read())
+
+	# Process template with variables
+	processed = template.render(
+		url_for = url_for
+	)
+	
+	# Remove all ids and classes
+	processed = re.sub(r"{:(.*?)}", "", processed)
+
+	return Response(processed, mimetype="text/markdown")
