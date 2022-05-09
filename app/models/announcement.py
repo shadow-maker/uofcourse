882a8c64942299498fb2669a230ecd8ac6fcec29
Carlos Andres <carlos.andres.montoro@gmail.com>
2022-05-09 01:49:29 -0600

    Make announcement title not null

--- app/models/announcement.py
+++ app/models/announcement.py
@@ -5,7 +5,7 @@ class Announcement(db.Model):
 	__tablename__ = "announcement"
 	id = db.Column(db.Integer, primary_key=True)
 	user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-	title = db.Column(db.String(256))
+	title = db.Column(db.String(256), nullable=False)
 	body = db.Column(db.Text)
 
 	read_by = db.relationship("User", secondary=UserAnnouncement, backref="read_announcements")
