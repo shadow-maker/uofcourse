abff7903e126e4b098e4474f2117f7a6007b4d11
Carlos Andres <carlos.andres.montoro@gmail.com>
2022-05-10 03:29:56 -0600

    Type hints

--- app/ifttt.py
+++ app/ifttt.py
@@ -6,19 +6,19 @@ import requests
 class IFTTT:
 	BASE_URL = "https://maker.ifttt.com/trigger/"
 
-	def __init__(self, key, events):
+	def __init__(self, key: str, events: dict[str, str]):
 		self.KEY = key
 		self.EVENTS = events
 	
-	def post(self, event, data={}):
+	def post(self, event: str, data: dict = {}) -> requests.Response:
 		url = self.BASE_URL + f"{event}/with/key/{self.KEY}"
 		r = requests.post(url, json=data)
 		return r
 	
-	def args(self, *args):
+	def args(self, *args) -> dict[str, str]:
 		return {f"value{i + 1}" : arg for i, arg in enumerate(args)}
 	
-	def message(self, body):
+	def message(self, body: str) -> requests.Response:
 		return self.post(self.EVENTS["message"], self.args(
 			f"[{SITE_NAME}] - New message received",
 			body.replace("\n", "<br>")
