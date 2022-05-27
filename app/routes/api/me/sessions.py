80fda3971718925083703acdab0981f536449eec
Carlos Andres <carlos.andres.montoro@gmail.com>
2022-05-26 20:01:16 -0600

    Simplify route

--- app/routes/api/me/sessions.py
+++ app/routes/api/me/sessions.py
@@ -4,36 +4,25 @@ import json
 
 me_session = Blueprint("sessions", __name__, url_prefix="/sessions")
 
+ALLOWED_SESSION_KEYS = [
+	"welcome",
+	"transferred"
+]
+
 #
 # PUT
 #
 
-@me_session.route("/welcome", methods=["PUT"])
-def putSessionWelcome():
+@me_session.route("/<key>", methods=["PUT"])
+def putSessionKey(key):
+	if key not in ALLOWED_SESSION_KEYS:
+		return {"error": "invalid session key"}, 400
 	data = request.form.to_dict()
 	if not "set" in data:
-		if "welcome" in session and session["welcome"] == False:
-			session["welcome"] = True
-		else:
-			session["welcome"] = False
+		session[key] = key in session and session[key] == False
 	else:
 		try:
-			session["welcome"] = bool(json.loads(data["set"]))
-		except:
-			return {"error": "invalid 'set' value in data"}, 400
-	return {"success": True}, 200
-
-@me_session.route("/transferred", methods=["PUT"])
-def putSessionTransferred():
-	data = request.form.to_dict()
-	if not "set" in data:
-		if "transferred" in session and session["transferred"] == False:
-			session["transferred"] = True
-		else:
-			session["transferred"] = False
-	else:
-		try:
-			session["transferred"] = bool(json.loads(data["set"]))
+			session[key] = bool(json.loads(data["set"]))
 		except:
 			return {"error": "invalid 'set' value in data"}, 400
 	return {"success": True}, 200
