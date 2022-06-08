6817b49fb8b5606707306a297a453898565b116d
Carlos Andres <carlos.andres.montoro@gmail.com>
2022-06-07 21:21:57 -0600

    Add endpoint for setting units needed

--- app/routes/api/me/main.py
+++ app/routes/api/me/main.py
@@ -1,7 +1,7 @@
-from app.models import User
+from app import db
 from app.auth import current_user
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 me_main = Blueprint("main", __name__, url_prefix="/")
 
@@ -20,5 +20,23 @@ def getProgress():
 		"courses_planned": current_user.coursesPlanned,
 		"units_taken": current_user.unitsTaken,
 		"units_planned": current_user.unitsPlanned,
-		"units_needed": float(current_user.units) if current_user.units else None
+		"units_needed": current_user.unitsNeeded
 	}, 200
+
+#
+# PUT
+#
+
+@me_main.route("/progress", methods=["PUT"])
+def putUnitsNeeded(unitsNeeded=None):
+	if unitsNeeded is None:
+		try:
+			unitsNeeded = float(request.form.get("units_needed"))
+		except:
+			return {"error": "invalid units_needed value"}, 400
+	try:
+		current_user.units = unitsNeeded
+		db.session.commit()
+	except:
+		return {"error": "failed to update units needed"}, 400
+	return {"success": True}, 200
