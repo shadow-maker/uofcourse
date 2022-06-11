4cc9ed72bf336e8f3df2aae0ef3a3f341eb4b298
Carlos Andres <carlos.andres.montoro@gmail.com>
2022-06-10 18:24:43 -0600

    Allow announcement passed in URL param to be shown even if its not in the current page

--- app/static/scripts/announcements.js
+++ app/static/scripts/announcements.js
@@ -2,26 +2,62 @@
 // GLOBAL VARS
 //
 
+var announcements = []
+
 // Init Page object (defined in pagination.html)
 var page = new Page(0, () => {
-	requestResults(updateResults)
+	getAnnouncements((data) => {
+		announcements = data.results
+		updateResults()
+		page.current = data.page
+		page.total = data.pages
+	
+		page.updateNav()
+
+		// Announcement passed in URL query parameter
+		if (selAnnouncement) {
+			let announcement = announcements.find(a => a.id == selAnnouncement)
+			if (announcement) { // Announcement in current page data
+				$("#modalInfoAnnouncement").modal("show")
+				loadModal(announcement)
+			} else { // Announcement not in current page data
+				getAnnouncement(selAnnouncement, (data) => {
+					$("#modalInfoAnnouncement").modal("show")
+					loadModal(data)
+				})
+			}
+			// Reset selAnnouncement
+			selAnnouncement = null
+			// Remove URL query parameter
+			window.history.pushState({}, document.title, window.location.pathname)
+		}
+	})
 })
 
 //
 // REQUEST FUNCS
 // 
 
-function requestResults(callback) {
+function getAnnouncements(callback) {
 	$.ajax({
 		url: "/api/announcements",
 		method: "GET",
 		data: {
 			sort: ["datetime"],
 			asc: false,
-			limit: 8,
+			limit: 10,
 			page: page.current
 		},
 		traditional: true,
+		success: callback,
+		error: displayError
+	})
+}
+
+function getAnnouncement(id, callback) {
+	$.ajax({
+		url: "/api/announcements/" + id,
+		method: "GET",
 		success: callback,
 		error: displayError
 	})
@@ -38,56 +74,45 @@ function putRead(id, set, callback) {
 }
 //
 // UPDATE FUNCS
-// 
+//
 
-function updateResults(data) {
+function loadModal(announcement) {
+	const modalInfo = $("#modalInfoAnnouncement")
+	modalInfo.find(".title").text(announcement.title)
+	modalInfo.find(".datetime").text(announcement.datetime.replace("T", " "))
+	modalInfo.find(".body").text(announcement.body)
+	modalInfo.find(".id").text(announcement.id)
+	
+	if (isAuth && !announcement.read) {
+		putRead(announcement.id, true, (response) => {
+			announcement.read = response.read
+			if (announcement.element && announcement.read)
+				announcement.element.find(".card-header").removeClass("alert-info").addClass("bg-light")
+		})
+	}
+}
+
+function updateResults() {
 	$(".loading").hide()
 	$(".loaded").show()
 
 	$("#announcementsContainer").empty()
-	for (let announcement of data.results) {
-		let item = $("#templates .announcement-item").clone()
+	for (let announcement of announcements) {
+		announcement.element = $("#templates .announcement-item").clone()
 
-		item.find(".announcement-title").text(announcement.title)
-		item.find(".announcement-time").text(announcement.datetime.replace("T", " "))
-		item.find(".announcement-text").text(announcement.body)
+		announcement.element.find(".announcement-title").text(announcement.title)
+		announcement.element.find(".announcement-time").text(announcement.datetime.replace("T", " "))
+		announcement.element.find(".announcement-text").text(announcement.body)
 
 		if (isAuth && !announcement.read)
-			item.find(".card-header").removeClass("bg-light").addClass("alert-info")
+		announcement.element.find(".card-header").removeClass("bg-light").addClass("alert-info")
 
-		item.find(".card-header").click(e => {
-			const modalInfo = $("#modalInfoAnnouncement")
-			modalInfo.find(".title").text(announcement.title)
-			modalInfo.find(".datetime").text(announcement.datetime.replace("T", " "))
-			modalInfo.find(".body").text(announcement.body)
-			modalInfo.find(".id").text(announcement.id)
-
-			item.find(".card-header").removeClass("alert-info").addClass("bg-light")
-			
-			if (isAuth && !announcement.read) {
-				putRead(announcement.id, true, (response) => {
-					item.find(".card-header").removeClass("alert-info").addClass("bg-light")
-					announcement.read = response.read
-				})
-			}
+		announcement.element.find(".card-header").on("click", () => {
+			loadModal(announcement)
 		})
-	
-		item.appendTo("#announcementsContainer")
 
-		if (announcement.id == announcementID) {
-			var modalInfo = $("#modalInfoAnnouncement")
-			modalInfo.find(".title").text(announcement.title)
-			modalInfo.find(".datetime").text(announcement.datetime.replace("T", " "))
-			modalInfo.find(".body").text(announcement.body)
-			modalInfo.find(".id").text(announcement.id)
-			modalInfo.modal("show")
-		}
+		announcement.element.appendTo("#announcementsContainer")
 	}
-
-	page.current = data.page
-	page.total = data.pages
-
-	page.updateNav()
 }
 //
 // DOCUMENT READY
