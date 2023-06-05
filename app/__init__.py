babf5ec44800449cc9c11eb7926c254a03c1d21d
Carlos Andres <carlos.andres.montoro@gmail.com>
2023-06-05 01:26:04 -0500

    Logger config

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from app.config import Config
-from app.constants import IFTTT_EVENTS
+from app.constants import IFTTT_EVENTS, LOG_FORMAT, LOG_DATE_FORMAT
+from app.localdt import utc
 from app.ifttt import IFTTT
 
 from flask import Flask
@@ -12,6 +13,8 @@ from flask_login import LoginManager
 from sqlalchemy.ext.declarative import DeclarativeMeta
 
 from jinja2 import Environment as JinjaEnvironment
+
+import logging
 
 #
 # Init Flask app
@@ -41,6 +44,23 @@ jinja = JinjaEnvironment()
 ifttt = IFTTT(app.config["IFTTT_KEY"], IFTTT_EVENTS)
 
 ipcache : dict[str, dict] = {}
+
+#
+# Logging
+#
+
+LOG_LEVEL = logging.INFO
+
+logger = logging.getLogger(__name__)
+logger.setLevel(LOG_LEVEL)
+
+logFormatter = logging.Formatter(LOG_FORMAT, LOG_DATE_FORMAT)
+logging.Formatter.converter = utc.converter()
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+
+logger.addHandler(consoleHandler)
 
 #
 # Import models and routes
