2f5602a95204e7ca8eb625ebdf37c3d9b650b5fb
Carlos Andres <carlos.andres.montoro@gmail.com>
2023-06-06 01:33:50 -0500

    Quickfix: constants not passed to errorhandler

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,11 +2,13 @@
 
 from app import app
 
+from app import constants
+from app.constants import ERROR_MESSAGES
+from app.models import Role, Faculty
 from app.routes.api import api
-from app.routes.views import view, constants
+from app.routes.views import view
 from app.routes.admin import admin
 from app.routes.files import file
-from app.constants import ERROR_MESSAGES
 
 from flask import request, render_template
 from werkzeug.exceptions import HTTPException
@@ -16,8 +18,26 @@ app.register_blueprint(api)
 app.register_blueprint(view)
 app.register_blueprint(file)
 
+#
+# Constants passed to all HTML templates to be accessed via Jinja2
+#
 
+@app.context_processor
+def viewConstants():
+	return {**{k : v for k, v in constants.__dict__.items() if not k.startswith("__")}, **{
+		"DEBUG": app.debug,
+		"GANALYTICS_ID" : app.config["GANALYTICS_ID"],
+		"GADSENSE_ID" : app.config["GADSENSE_ID"],
+		"PROPELLER_ID" : app.config["PROPELLER_ID"],
+		"ROLE_ADMIN": Role.admin,
+		"ROLE_MOD": Role.moderator,
+		"FACULTIES": Faculty.query.all(),
+	}}
+
+#
 # Error handler for all routes
+#
+
 @app.errorhandler(HTTPException)
 def errorHandler(error):
 	if request.path.startswith(api.url_prefix): # API route
@@ -25,7 +45,6 @@ def errorHandler(error):
 			"error": ERROR_MESSAGES[error.code] if error.code in ERROR_MESSAGES else error.description
 		}, error.code
 	return render_template("error.html",
-		**constants,
 		title = error.code,
 		errorCode = error.code,
 		errorMessage = ERROR_MESSAGES[error.code] if error.code in ERROR_MESSAGES else error.description
--- app/routes/views/__init__.py
+++ app/routes/views/__init__.py
@@ -1,6 +1,3 @@
-from app import app
-from app import constants
-from app.models import Role, Faculty
 from flask import Blueprint
 
 #
@@ -8,22 +5,6 @@ from flask import Blueprint
 #
 
 view: Blueprint = Blueprint("view", __name__, url_prefix="/")
-
-#
-# Constants passed to all HTML templates to be accessed via Jinja2
-#
-
-@view.context_processor
-def viewConstants():
-	return {**{k : v for k, v in constants.__dict__.items() if not k.startswith("__")}, **{
-		"DEBUG": app.debug,
-		"GANALYTICS_ID" : app.config["GANALYTICS_ID"],
-		"GADSENSE_ID" : app.config["GADSENSE_ID"],
-		"PROPELLER_ID" : app.config["PROPELLER_ID"],
-		"ROLE_ADMIN": Role.admin,
-		"ROLE_MOD": Role.moderator,
-		"FACULTIES": Faculty.query.all(),
-	}}
 
 #
 # Import all view routes
